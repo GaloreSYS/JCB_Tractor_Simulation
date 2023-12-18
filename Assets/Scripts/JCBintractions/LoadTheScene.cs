dcc905bef3001b93398a7bd548b9705b2af184b6
ashishtanwar <65059656+Yokesh-4040@users.noreply.github.com>
2023-12-18 08:11:45 +0530

    Final Changes

---
+++ Assets/Scripts/JCBintractions/LoadTheScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTheScene : MonoBehaviour
+{
+    [Header ("Example 0 to 9")]
+    public int SceneNumber;
+
+    public void OnClickLoadTheScene(int value)
+    {
+        
+        SceneNumber = value;
+        SceneManager.LoadScene(SceneNumber);
+    }
+}
