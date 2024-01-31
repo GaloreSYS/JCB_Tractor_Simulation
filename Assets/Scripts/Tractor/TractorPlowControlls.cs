9abb3a5adc903abc8eac232f24d86220ae9c0283
GaloreSYS <111342605+GaloreSYS@users.noreply.github.com>
2024-01-31 11:33:18 +0530

    wednesday
    working

--- Assets/Scripts/Tractor/TractorPlowControlls.cs
+++ Assets/Scripts/Tractor/TractorPlowControlls.cs
@@ -6,9 +6,14 @@ public class TractorPlowControlls : MonoBehaviour
 {
     public Animator AnimTractorController;
     public ArmDataJCB Tractor;
+    public GameObject[] boxs;
+    public GameObject testground;
     public bool EnableDown, EnableUp;
-    public float ValueTractor;
-
+    public float ValueTractor; 
+    public RaycastHit hit;
+    public float rayLength = 0.5f;
+    public bool onGround = false;
+    
     private void Start()
     {
         ValueTractor = 0;
@@ -27,6 +32,7 @@ public class TractorPlowControlls : MonoBehaviour
             {
                 ValueTractor -= Time.deltaTime * Tractor.TractorPlow;
                 EnableUp = false;
+
             }
         }
 
@@ -42,5 +48,33 @@ public class TractorPlowControlls : MonoBehaviour
 
             Tractor.valueTractor = ValueTractor;
         AnimTractorController.SetFloat("TP", ValueTractor);
+
+
+
+        //
+
+        Ray checkGround = new Ray(testground.transform.position, Vector3.down);
+
+        Debug.DrawRay(testground.transform.position, Vector3.down * rayLength);
+        if (Physics.Raycast(checkGround, out hit, rayLength))
+        {
+            if (hit.collider.tag == "ground")
+            {
+                onGround = true;
+
+                boxs[0].gameObject.SetActive(true);
+                boxs[1].gameObject.SetActive(true);
+                boxs[2].gameObject.SetActive(true);
+            }
+            if (EnableUp == true)
+            {
+                boxs[0].gameObject.SetActive(false);
+                boxs[1].gameObject.SetActive(false);
+                boxs[2].gameObject.SetActive(false);
+
+            }
+        }
+
     }
+
 }
