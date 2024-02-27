447818b05eb4569c22e7f9878066a79b45f1cbdf
GaloreSYS <111342605+GaloreSYS@users.noreply.github.com>
2024-02-27 14:46:43 +0530

    Revert "ConveyrFixed"
    This reverts commit 157bb26d0f3608d0199a3abb85d0c10f03ad5715.

--- Assets/4040/BailerScripts/ConvyerBelt2.cs
+++ Assets/4040/BailerScripts/ConvyerBelt2.cs
@@ -1,27 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class ConvyerBelt2 : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] float Speed;
     [SerializeField] bool startConv;
-    [SerializeField] Rigidbody[] HeyRiggidBody;
-    [SerializeField] PositionConstraint[] HeyConstraint;
+
     private void Start()
     {
         startConv = false;
-        for(int i= 0; i <HeyRiggidBody.Length; i++)
-        {
-            HeyRiggidBody[i].isKinematic = true;
-        }
-
-        for (int i = 0; i < HeyConstraint.Length; i++)
-        {
-            HeyConstraint[i].constraintActive = true;
-        }
     }
     public void ConveyerMethod()
     {
@@ -34,15 +23,6 @@ public class ConvyerBelt2 : MonoBehaviour
         if(startConv == true)
         {
             ConveyerMethod();
-            for (int i = 0; i < HeyRiggidBody.Length; i++)
-            {
-                HeyRiggidBody[i].isKinematic = false;
-            }
-
-            for (int i = 0; i < HeyConstraint.Length; i++)
-            {
-                HeyConstraint[i].constraintActive = false;
-            }
         }
     }
 }
